541677e27d810895b14aace6062fcf0a140a1ab7
John <johnranierbueno@gmail.com>
2026-06-03 23:11:53 -0700

    updated workout controller

--- server/src/controllers/workoutController.js
+++ server/src/controllers/workoutController.js
@@ -15,32 +15,47 @@ async function getAllWorkouts(req, res) {
 
 async function getWorkoutById(req, res) {
 	const workoutId = Number(req.params.id);
-	const workout = workouts.find((item) => item.id === workoutId);
+	// const workout = workouts.find((item) => item.id === workoutId);
 
-	if (!workout) {
-		return res.status(404).json({ message: "Workout not found" });
+	try {
+		const workoutResult = await pool.query(
+			`SELECT id, name, workout_date, notes, created_at FROM workouts WHERE id = $1`,
+			[workoutId],
+		);
+
+		if (workoutResult.rows.length === 0) {
+			return res.status(404).json({ message: "Workout not found" });
+		}
+
+		return res.status(200).json(workoutResult.rows[0]);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ message: "Failed to fetch workout" });
 	}
-
-	return res.status(200).json(workout);
 }
 
-function createWorkout(req, res) {
+async function createWorkout(req, res) {
 	const { name, date, notes } = req.body;
 
 	if (!name || !date) {
 		return res.status(400).json({ message: "Name and date are required" });
 	}
 
-	const newWorkout = {
-		id: workouts.length + 1,
-		name,
-		date,
-		notes: notes || "",
-	};
+	try {
+		const result = await pool.query(
+			`
+            INSERT INTO workouts (name, workout_date, notes)
+            VALUES ($1, $2, $3)
+            RETURNING id, name, workout_date, notes, created_at
+            `,
+			[name, date, notes || ""],
+		);
 
-	workouts.push(newWorkout);
-
-	return res.status(201).json(newWorkout);
+		return res.status(201).json(result.rows[0]);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ message: "Failed to create workout" });
+	}
 }
 
 module.exports = {
